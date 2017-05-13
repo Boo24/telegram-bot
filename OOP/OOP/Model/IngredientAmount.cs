eae40197230c3f07ec3afa5cdded461124e4bc5a
Boo24 <bububu241@gmail.com>
2017-05-13 17:01:57 +0500

    Add files via upload

---
+++ OOP/OOP/Model/IngredientAmount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Model
+{
+    class IngredientAmount: IIngredientAmount
+
+
+    {
+    public string MeasureUnit { get; private set; }
+    public double Count { get; private set; }
+
+
+    public void Change(double delta)
+    {
+        Count += delta;
+    }
+
+
+    }
+}
